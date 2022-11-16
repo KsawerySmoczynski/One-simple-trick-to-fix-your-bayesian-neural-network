ed307e0ddd24dca20b4536bab5fc6f6ec83e5698
KsawerySmoczzynski <k.smocz@gmail.com>
2022-11-16 15:40:34 +0100

    temp commit

--- scripts/bayesian_experiments.sh
+++ scripts/bayesian_experiments.sh
@@ -11,7 +11,7 @@ ACTIVATIONS=$(echo "configs/bayesian/activation/*${ACTIVATIONS_PATTERN}*")
 
 run_experiments(){
 	DATASETS=${1}
-	MODELS=${2}
+	MODELS=${2}w
 	ACTIVATIONS=${3}
 	for DATASET_CONFIG in ${DATASETS}
 	do
@@ -31,8 +31,8 @@ if [ ${MODE} == "1ch" ] || [ ${MODE} == "all" ]; then
 	run_experiments "${DATASETS_1CH}" "${MODELS_1CH}" "${ACTIVATIONS}"
 fi
 
-if [ ${MODE} == "3ch" ] || [ ${MODE} == "all" ]; then
-	run_experiments "${DATASETS_3CH}" "${MODELS_3CH}" "${ACTIVATIONS}"
-fi
+# if [ ${MODE} == "3ch" ] || [ ${MODE} == "all" ]; then
+# 	run_experiments "${DATASETS_3CH}" "${MODELS_3CH}" "${ACTIVATIONS}"
+# fi
 
 } 2>&1 | ts | tee run.log
